4b9701f5bf1b86e30ae70a183e0c2e007e3568da
Ataliba Teixeira <ataliba@ataliba.eti.br>
2012-03-30 22:08:05 -0300

    First lines of the f.lux install

--- f.lux-install/install.sh
+++ f.lux-install/install.sh
@@ -5,26 +5,49 @@
 # 
 #         USAGE:  ./install.sh 
 # 
-#   DESCRIPTION:  
+#   DESCRIPTION:  A simple installation script for the xflux linux binary  
 # 
-#       OPTIONS:  ---
-#  REQUIREMENTS:  ---
-#          BUGS:  ---
-#         NOTES:  ---
-#        AUTHOR:   (), 
-#       COMPANY:  
-#       VERSION:  1.0
+#  REQUIREMENTS:  Shell script, wget and internet connection 
+#          BUGS:  
+#         NOTES:  
+#        AUTHOR:  Ataliba Teixeira  (ataliba@ataliba.eti.br)
+#       VERSION:  0.2
 #       CREATED:  03/30/2012 09:46:15 PM BRT
-#      REVISION:  ---
+#      REVISION:  1
 #===============================================================================
 
+TheDir=/tmp/f.lux
 
 # Creating the f.lux temp directory and download the xflux binary 
 
-mkdir /tmp/f.lux
+mkdir $TheDir 
 
-cd /tmp/f.lux
+cd $TheDir 
 
 wget http://secure.herf.org/flux/xflux.tgz
+
+
+# Asking for your address and getting the coordinates on Maps API
+
+echo -n "Give me your address / Manda aí o seu endereço "
+read Maps 
+
+cd $TheDir 
+
+curl ""
+
+#Looking for four keywords in here
+for key in changelog lat lng
+do
+OUTPT=`grep $key /tmp/geodecode.xml | tr -d '\t' | sed 's/^\([^<].*\)$/\1/' `
+eval ${key}=`echo -ne \""${OUTPT}"\"`
+done
+
+# Getting the results in specific arrays
+lat=( `echo ${lat}` )
+lng=( `echo ${lng}` )
+
+echo ${lng[@]} | awk -F">" '{print $2}' | awk -F"<" '{print $1}'
+echo ${lat[@]} | awk -F">" '{print $2}' | awk -F"<" '{print $1}'
 
 
